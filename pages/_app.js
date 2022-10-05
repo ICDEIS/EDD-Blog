15e3006cd0f12b93b39edff16c52b34154316cce
IC Deis <eddess@IC-Deis.AC800S>
2022-10-05 09:12:30 +0500

    some fixing

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,11 @@
 import Layout from '../component/Layout'
 import '../styles/globals.scss'
 import 'bootstrap/dist/css/bootstrap.css'
-import '../www/style//esta.scss'
-import '../www/style/initial.scss'
-import '../component/Layout/layout.scss'
+import '../styles/esta.scss'
+import '../styles/initial.scss'
+import '../styles/layout.scss'
+import '../styles/loadere.scss'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import '../www/ui/Loader/loadere.scss'
 import Section from '../component/layout/Section'
 
 function MyApp({ Component, pageProps }) {
